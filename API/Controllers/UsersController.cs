7d468f3189da5f96e2863a62a5d8ad99e80179a6
DeJSmith <djssmith@hotmail.co.nz>
2020-10-04 13:20:17 +1300

    Finished Section 3 building a walking skeleton part 2"

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Controllers
 {
     [ApiController]
-    [Route("api/[controller")]
+    [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
