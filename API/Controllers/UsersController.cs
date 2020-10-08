576df814c1b00f7a3708b932ac9e35dda8cec7b1
DeJSmith <djssmith@hotmail.co.nz>
2020-10-08 14:03:06 +1300

    Finished Section 13: Paging, Sorting and Filtering - added a query builder to the server that allows us to build up a query object with desired params - added paginiation on client - added filtering on client - added cahcing compatible with filter/sorting etc.. on client

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,6 +8,7 @@ using Data;
 using Dtos;
 using Entities;
 using Extensions;
+using Helpers;
 using Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -32,9 +33,20 @@ namespace Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
         {
-            var users = await _userRepository.GetMembersAsync();
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            userParams.CurrentUsername = User.GetUsername();
+
+            if (string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = user.Gender == "male" ? "female" : "male";
+            }
+
+            var users = await _userRepository.GetMembersAsync(userParams);
+
+            Response.AddPaginationHeader(users.CurrentPage, userParams.PageSize,
+                                         users.TotalCount, users.TotalPages);
 
             return Ok(users);
         }
@@ -116,6 +128,7 @@ namespace Controllers
 
             return BadRequest("Failed to set main photo");
         }
+
 
         [HttpDelete("delete-photo/{photoId}")]
         public async Task<ActionResult> DeletePhoto(int photoId)
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { Member } from 'src/app/_models/member';
+import { Pagination } from 'src/app/_models/pagination';
+import { User } from 'src/app/_models/user';
+import { UserParams } from 'src/app/_models/userParams';
+import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -9,11 +14,40 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]>;
+  members: Member[];
+  pagination: Pagination;
+  userParams: UserParams;
+  user: User;
+  pageinationBtnClasses = 'btn-info';
+  genderList = [
+    { value: 'male', display: 'Males' },
+    { value: 'female', display: 'Females' },
+  ];
 
-  constructor(private _memberService: MembersService) {}
+  constructor(private _memberService: MembersService) {
+    this.userParams = this._memberService.getUserParams();
+  }
 
   ngOnInit(): void {
-    this.members$ = this._memberService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this._memberService.setUserParams(this.userParams);
+    this._memberService.getMembers(this.userParams).subscribe((response) => {
+      this.members = response.result;
+      this.pagination = response.pagination;
+    });
+  }
+
+  resetFilters() {
+    this.userParams = this._memberService.resetUserParams();
+    this.loadMembers();
+  }
+
+  pageChanged(event: any) {
+    this.userParams.pageNumber = event.page;
+    this._memberService.setUserParams(this.userParams);
+    this.loadMembers();
   }
 }
